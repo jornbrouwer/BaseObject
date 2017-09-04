5d4540e37638036e9d4d3f9458d89b81a0037daa
Jorn Brouwer <Jorn.brouwer@gmail.com>
2017-09-04 16:15:03 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 4 Sep 2017
+ *      Author: jornbrouwer
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "program started" << std::endl;
+	return 0;
+}
+
+
+
+
+
